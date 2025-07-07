a843a78e09bd155bb7d2a997d4dae7a4a5d64f56
Leyla-spec <leylasa@code.edu.az>
2025-07-07 17:17:45 +0400

    Added Project2
    Initialize solution with layered structure

---
+++ Project2.PL/Project2.Core/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using Project2.Core.Enumns;
+
+namespace Project2.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Categories Categories { get; set; }
+    }
+}
